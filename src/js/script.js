f6034f5f6617166eba0188c48ff19a55a0c94354
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-10-02 11:36:48 -0500

    addBookToLibrary function

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,14 @@ function Book(title, author, numPages, read) {
   this.numPages = numPages;
   this.read = read;
 }
+
+function addBookToLibrary() {
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let numPages = document.querySelector("#numPages").value;
+  let read = document.querySelector("#read").value;
+
+  let newBook = new Book(title, author, numPages, read);
+  myLibrary.push(newBook);
+  alert(newBook.title + " was added successfully !!!");
+}
